536fe68e6151ab1c47dcd6aec47b4cc4389231cd
Agustin Guarino <agustinguarino01@gmail.com>
2025-02-15 19:21:06 -0300

    Initial commit

---
+++ ListingsExtractor.py
@@ -0,0 +1,63 @@
+import requests
+from lxml.html import fromstring
+
+# TODO: Define list of all types of properties wanted for scrape (apartamentos, casa, cochera, etc) and get URLs
+# TODO: Define list of all neighborhoods and get slugs
+# TODO: Create process to get all URLs needed for scrape by mixing types of properties and neighborhoods
+
+url = 'https://listado.mercadolibre.com.uy/inmuebles/apartamentos/venta/montevideo/aguada/_Desde_%index%_NoIndex_True'
+index = 1
+results = []
+
+def extract_value(soup, locator, index):
+    value = ""
+    try:
+        return soup.xpath(locator)[index].text_content()
+    except Exception:
+        return "Unknown"
+
+while True:
+    request_url = url.replace("%index%", str(index))
+    print(f"[!] Sending request to: {request_url}")
+    response = requests.get(request_url)
+
+    index += 48
+
+    soup = fromstring(response.text)
+
+    main_selector = "//div[@class='poly-card__content']"
+    listings = soup.xpath(main_selector)
+
+    print(f"[!] Listings detected: {str(len(listings))}.")
+
+    if len(listings) > 0:
+        print("[!] Starting scrapping.")
+        for i in range(0, len(listings)):
+            print(f"Iteration: {str(i)}")
+            title_selector = f"//h3[@class='poly-component__title-wrapper']"
+            link_selector = f"{title_selector}//a"
+            price_selector = f"//span[@class='andes-money-amount__fraction']"
+            location_selector = f"//span[@class='poly-component__location']"
+            details_delector = f"//div[@class='poly-component__attributes-list']"
+
+            title = soup.xpath(title_selector)[i].text_content()
+            link = soup.xpath(link_selector)[i].get('href')
+            price = soup.xpath(price_selector)[i].text_content()
+            location = soup.xpath(location_selector)[i].text_content()
+            details =  extract_value(soup, details_delector, i) #TODO: Create alert for these cases
+            print(f"Title: {title} // Price: {price} // Details: {details} // Location: {location} // Link: {link} \n\n")
+
+            result = {
+                "title": title,
+                "link": link,
+                "price": price,
+                "location": location,
+                "details": details
+            }
+
+            results.append(result)
+    else:
+        print("[!] No more listing available. Stopping scrapping.")
+        break
+
+print(f"[!] Final count of results: {str(len(results))}.")
