40f04c93a39d0220929c3a7fac14f1adb38c2837
Agustin Guarino <agustinguarino01@gmail.com>
2025-02-20 15:49:38 -0300

    Changed structure to insert record

--- Extractors/Utils/DB.py
+++ Extractors/Utils/DB.py
@@ -1,29 +1,23 @@
-import sqlite3
+import sqlite3, os.path
 #from Util import Util
 
 class DB:
     def __init__(self):
-        cursor = self.__start_connection()
+        self.connection = self.__start_connection()
+        self.BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     def __start_connection(self):
         # TODO: Read db name from config
         # connection = sqlite3.connect(Util().get_db_name())
-        connection = sqlite3.connect('inmobilis.db')
-        self.connection = connection
-        return self.connection.cursor()
+        connection = sqlite3.connect(os.path.join(os.path.dirname(os.path.abspath(__file__)), "inmobilis.db"))
+        return connection
     
     def insert_listing(self, listing:dict):
         # TODO: Read query from config, throwing error when importing Util for some reason
-        query = "INSERT INTO listings (Title, Link, Raw_Link, Price, Address, Raw_Details, Bedrooms, Bathrooms, Area, Property_Type, Neighborhood, Operation_Type, Scrape_Id) VALUES (%Title%, %Link%, %Raw_Link%, %Price%, %Address%, %Raw_Details%, 0, 0, 0, %Property_Type%, %Neighborhood%, %Operation_Type%, 0)"
-
-        query = query.replace("%Title%", listing['title'])
-        query = query.replace("%Link%", listing['link'])
-        query = query.replace("%Raw_Link%", listing['raw_link'])
-        query = query.replace("%Price%", listing['price'])
-        query = query.replace("%Address%", listing['address'])
-        query = query.replace("%Raw_Details%", listing['raw_details'])
-        query = query.replace("%Property_Type%", listing['property_type'])
-        query = query.replace("%Neighborhood%", listing['neighborhood'])
-        query = query.replace("%Operation_Type%", listing['operation_type'])
+        # query = "INSERT INTO listings (Title, Link, Raw_Link, Price, Address, Raw_Details, Bedrooms, Bathrooms, Area, Property_Type, Neighborhood, Operation_Type, Scrape_Id) VALUES (%Title%, %Link%, %Raw_Link%, %Price%, %Address%, %Raw_Details%, 0, 0, 0, %Property_Type%, %Neighborhood%, %Operation_Type%, 0)"
+        query = "INSERT INTO listings (Title, Link, Raw_Link, Price, Address, Raw_Details, Bedrooms, Bathrooms, Area, Property_Type, Neighborhood, Operation_Type, Scrape_Id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
         print(query)
+
+        self.connection.cursor().execute(query, (listing['title'], listing['link'], listing['raw_link'], listing['price'], listing['address'], listing['raw_details'], 0, 0, 0, listing['property_type'], listing['neighborhood'], listing['operation_type'], 0))
+        self.connection.commit()
