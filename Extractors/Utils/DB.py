009fb2212af58c225a6905d74364090bb093ef53
Agustin Guarino <agustinguarino01@gmail.com>
2025-02-20 14:41:44 -0300

    Created function to start connection

--- Extractors/Utils/DB.py
+++ Extractors/Utils/DB.py
@@ -1,5 +1,10 @@
 import sqlite3
+from Util import Util
 
 class DB:
     def __init__(self):
         pass
+
+    def __start_connection(self):
+        connection = sqlite3.connect(Util().get_db_name())
+        return connection.cursor()
