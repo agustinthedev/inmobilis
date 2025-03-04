8e2663c0e7dd66bb4d86bae39b76e6bea3f8d58c
Agustin Guarino <agustinguarino01@gmail.com>
2025-03-03 21:51:17 -0300

    Implemented creation of new scrape id each time file is run

--- Runner.py
+++ Runner.py
@@ -1,12 +1,12 @@
-import subprocess
+import subprocess, multiprocessing
 from Extractors.Utils.DB import DB
 
 db = DB()
 
 commands = []
 
-scrape_id = "301"
-command = 'python .\Extractors\ListingsExtractor.py --url "%url%" --neighborhood %neighborhood% --scrapeid %scrape_id%'
+scrape_id = db.create_new_scrape_id()
+command = 'python .\Extractors\ListingsExtractor.py --url "%url%" --neighborhood "%neighborhood%" --scrapeid %scrape_id%'
 
 urls_to_scrape = db.get_urls_to_scrape()
 for url in urls_to_scrape:
@@ -20,3 +20,5 @@ print(f"Total commands: {str(len(commands))}")
 
 for command in commands:
     subprocess.Popen(command)
+
+print(f"[!] Results saved with scrape_id: {str(scrape_id)}.")
