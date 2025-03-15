9ae19ab6f90b10fd8680cc9e0ac3e329d8b3bb60
Agustin Guarino <agustinguarino01@gmail.com>
2025-03-15 00:56:16 -0300

    Initial structure for Telegram module

---
+++ Extractors/Utils/Telegram.py
@@ -0,0 +1,14 @@
+from telebot_router import TeleBot
+from Util import Util
+
+class Telegram:
+    def __init__(self):
+        self.app = TeleBot(__name__)
+        self.app.config['api_key'] = Util().get_telegram_bot_token()
+        self.chat_id = Util().get_telegram_chat_id()
+
+    def send_alert(self):
+        self.app.send_message(chat_id=self.chat_id, text="Hello\nAnother test")
+
+
+t = Telegram().send_alert()
