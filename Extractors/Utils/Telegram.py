91554e7d216364e7df9fd502734b57e06ff9586a
Agustin Guarino <agustinguarino01@gmail.com>
2025-04-06 22:54:04 -0300

    Changed argument to receive text to send in message

--- Extractors/Utils/Telegram.py
+++ Extractors/Utils/Telegram.py
@@ -7,5 +7,5 @@ class Telegram:
         self.app.config['api_key'] = Util().get_telegram_bot_token()
         self.chat_id = Util().get_telegram_chat_id()
 
-    def send_alert(self):
-        self.app.send_message(chat_id=self.chat_id, text="Hello\nAnother test")
+    def send_alert(self, text:str):
+        self.app.send_message(chat_id=self.chat_id, text=text)
