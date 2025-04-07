9d34c385fc0e0dd7b564f152b626fcde189082ca
Agustin Guarino <agustinguarino01@gmail.com>
2025-04-06 22:53:30 -0300

    Accomodated changes to support search by row id instead of getting all results for a scrape id

--- Extractors/Utils/Opportunities.py
+++ Extractors/Utils/Opportunities.py
@@ -3,29 +3,33 @@ from .Telegram import Telegram
 from .Util import Util
 
 class Opportunities:
-    def __init__(self, scrape_id):
+    def __init__(self, scrape_id, listings):
         self.scrape_id = scrape_id
+        self.listings_to_process = listings
         self.db = DB()
+        self.telegram = Telegram()
 
     def process_results(self):
-        listings = self.db.get_scrape_results_listings(scrape_id=self.scrape_id, operation_type="Venta")
+        for individual_listing in self.listings_to_process:
+            listings = self.db.get_listing_by_row_id(row_id=str(individual_listing), operation_type="Venta")
 
-        for listing in listings:
-            price = int(listing[4])
-            median_rent = self.db.get_median_price(
-                scrape_id=self.scrape_id,
-                operation="Alquiler",
-                neighborhood=str(listing[11]),
-                bedrooms=str(listing[7]),
-                exclude_types="Residencia"
-            )
-            roi_alert = self.db.get_roi_alert_number()
-            price_alert = self.db.get_price_alert_number()
-            dolar_price = Util().get_dolar_price()
+            for listing in listings:
+                price = int(listing[4])
+                median_rent = self.db.get_median_price(
+                    scrape_id=self.scrape_id,
+                    operation="Alquiler",
+                    neighborhood=str(listing[11]),
+                    bedrooms=str(listing[7]),
+                    exclude_types="Residencia"
+                )
+                roi_alert = self.db.get_roi_alert_number()
+                price_alert = self.db.get_price_alert_number()
+                dolar_price = Util().get_dolar_price()
 
-        if ((median_rent/dolar_price)*100)/price >= roi_alert or price <= price_alert:
-            if not DB().opportunity_sent_exists(url=listing[2]):
-                print(f"[!] Opportunity detected: {str(listing[0])}")
-                DB().insert_opportunity_sent(url=listing[2])
+                if ((median_rent/dolar_price)*100)/price >= roi_alert or price <= price_alert:
+                    if not DB().opportunity_sent_exists(url=listing[2]):
+                        print(f"[!] Opportunity detected: {str(listing[0])}")
+                        DB().insert_opportunity_sent(url=listing[2])
 
-                #TODO: Send Telegram message
+                        #TODO: Send Telegram message
+                        self.telegram.send_alert(url=listing[2])
